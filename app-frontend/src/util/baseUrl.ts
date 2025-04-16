d2ff313a1285dc5250d0ba37652bc225b758bf6a
Mixchelle <michelle.biologia@hotmail.com>
2025-04-16 15:02:16 -0300

    a

--- app-frontend/src/util/baseUrl.ts
+++ app-frontend/src/util/baseUrl.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = (): string => {
   console.log('oi')
-    return process.env.NEXT_PUBLIC_API_URL ||  "http://ip172-18-0-24-cvvud2i91nsg00dfnr2g-8000.direct.labs.play-with-docker.com/";
+    return process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/ " ;
   };
