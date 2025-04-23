21852c705c78e1691ab4bfc743f0cfa40d9fb5ee
Mixchelle <michelle.biologia@hotmail.com>
2025-04-23 14:15:33 -0300

    netlify

--- app-frontend/src/util/baseUrl.ts
+++ app-frontend/src/util/baseUrl.ts
@@ -1,3 +1,3 @@
 export const getBaseUrl = (): string => {
-    return process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+    return process.env.NEXT_PUBLIC_API_URL || "https://projeto-future-backend.onrender.com";
   }; 
